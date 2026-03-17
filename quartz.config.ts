ccdae6572419bb324295a504f46287d1f56b9f80
tammynpm <112338317+tammynpm@users.noreply.github.com>
2026-03-17 03:28:16 -0400

    fixed baseUrl to pages.dev

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz-t.ntammy947.workers.dev",
+    baseUrl: "quartz-t.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
